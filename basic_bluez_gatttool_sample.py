76e4605c9d2bc9c2953a54d081cd1ec8a449d1cc
justin.mak <justin.mak@xylem.com>
2021-08-13 15:14:17 +0100

    Set timeout to default

--- basic_bluez_gatttool_sample.py
+++ basic_bluez_gatttool_sample.py
@@ -11,7 +11,7 @@ device = adapter.connect(device_addr)
 for uuid in device.discover_characteristics().keys():
     while True:
         try:
-            print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid, timeout=10))))
+            print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid))))
         except:
             device = adapter.connect(device_addr)
             continue
