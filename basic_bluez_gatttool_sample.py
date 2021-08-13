53cd80827d52d3c248ce689c24dc87de733550ba
justin.mak <justin.mak@xylem.com>
2021-08-13 13:41:05 +0100

    Put try block in sample

--- basic_bluez_gatttool_sample.py
+++ basic_bluez_gatttool_sample.py
@@ -8,8 +8,12 @@ YOUR_DEVICE_ADDRESS = "80:6F:B0:F0:2B:95"
 ADDRESS_TYPE = pygatt.BLEAddressType.random
 
 adapter = pygatt.GATTToolBackend()
-adapter.start()
-device = adapter.connect(YOUR_DEVICE_ADDRESS, address_type=ADDRESS_TYPE)
 
-for uuid in device.discover_characteristics().keys():
-    print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid))))
+try:
+    adapter.start()
+    device = adapter.connect(YOUR_DEVICE_ADDRESS, address_type=ADDRESS_TYPE)
+
+    for uuid in device.discover_characteristics().keys():
+        print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid))))
+finally:
+    adapter.stop()
