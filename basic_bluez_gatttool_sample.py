7d67b3911539885d27dff0ffc492283317931209
justin.mak <justin.mak@xylem.com>
2021-08-13 13:46:34 +0100

    Remove try block

--- basic_bluez_gatttool_sample.py
+++ basic_bluez_gatttool_sample.py
@@ -8,12 +8,8 @@ YOUR_DEVICE_ADDRESS = "80:6F:B0:F0:2B:95"
 ADDRESS_TYPE = pygatt.BLEAddressType.public
 
 adapter = pygatt.GATTToolBackend()
+adapter.start()
+device = adapter.connect(YOUR_DEVICE_ADDRESS, address_type=ADDRESS_TYPE)
 
-try:
-    adapter.start()
-    device = adapter.connect(YOUR_DEVICE_ADDRESS, address_type=ADDRESS_TYPE)
-
-    for uuid in device.discover_characteristics().keys():
-        print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid))))
-finally:
-    adapter.stop()
+for uuid in device.discover_characteristics().keys():
+    print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid))))
