63601c0c85bc386cd1d0aff335c406b391641c73
justin.mak <justin.mak@xylem.com>
2021-08-13 15:01:06 +0100

    Update

--- basic_bluez_gatttool_sample.py
+++ basic_bluez_gatttool_sample.py
@@ -3,20 +3,17 @@ from __future__ import print_function
 import binascii
 import pygatt
 
-YOUR_DEVICE_ADDRESS = "80:6F:B0:F0:2B:95"
-# Many devices use random addressing - this is required to connect
-ADDRESS_TYPE = pygatt.BLEAddressType.public
-
+device_addr = "80:6F:B0:F0:2B:95"
 adapter = pygatt.GATTToolBackend()
 adapter.start()
-device = adapter.connect(YOUR_DEVICE_ADDRESS, address_type=ADDRESS_TYPE)
+device = adapter.connect(device_addr)
 
 for uuid in device.discover_characteristics().keys():
     while True:
         try:
             print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid, timeout=10))))
-        except:
-            device = adapter.connect(YOUR_DEVICE_ADDRESS, address_type=ADDRESS_TYPE)
+        except pygatt.exceptions.NotConnectedError:
+            device = adapter.connect(device_addr)
             continue
         
         break
