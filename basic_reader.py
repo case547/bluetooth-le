fdb0cd00274a379c02716166c652c06a933832c1
justin.mak <justin.mak@xylem.com>
2021-08-16 16:52:46 +0100

    Add sensor ids

--- basic_reader.py
+++ basic_reader.py
@@ -3,11 +3,19 @@ from binascii import hexlify
 
 adapter = pygatt.GATTToolBackend()
 
+sensor_ids = {
+    "temp_data": "f000aa01-0451-4000-b000-000000000000",
+    "humidity_data": "f000aa21-0451-4000-b000-000000000000",
+    "lux_data": "f000aa71-0451-4000-b000-000000000000",
+    "batt_lvl": "f0002a19-0451-4000-b000-000000000000"
+}
+
 try:
     adapter.start()
     device = adapter.connect('80:6F:B0:F0:2B:95')   # target MAC address
     while True:
-        value = device.char_read("f000aa71-0451-4000-b000-000000000000")    # luxometer data UUID
-        print(f"Received data: {hexlify(value)}")
+        value = device.char_read(sensor_ids["lux_data"])
+        # print(f"Received data: {hexlify(value)}")
+        print(value)
 finally:
     adapter.stop()
