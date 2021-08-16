1b93343bf87187a99b55a6f26a86c4993801a6cc
justin.mak <justin.mak@xylem.com>
2021-08-16 11:47:13 +0100

    Put calls in while loop

--- connect_example.py
+++ connect_example.py
@@ -5,8 +5,9 @@ adapter = pygatt.GATTToolBackend()
 
 try:
     adapter.start()
-    device = adapter.connect('80:6F:B0:F0:2B:95')   # target MAC address
-    value = device.char_read("f000aa01-0451-4000-b000-000000000000")    # temperature data UUID
-    print(f"Received data: {hexlify(value)}")
+    while True:
+        device = adapter.connect('80:6F:B0:F0:2B:95')   # target MAC address
+        value = device.char_read("f000aa01-0451-4000-b000-000000000000")    # temperature data UUID
+        print(f"Received data: {hexlify(value)}")
 finally:
     adapter.stop()
