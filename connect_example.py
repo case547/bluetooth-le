907e4bd02c486b65a1f5367bde19819960e85472
justin.mak <justin.mak@xylem.com>
2021-08-16 11:52:43 +0100

    Change to lux data

--- connect_example.py
+++ connect_example.py
@@ -7,7 +7,7 @@ try:
     adapter.start()
     device = adapter.connect('80:6F:B0:F0:2B:95')   # target MAC address
     while True:
-        value = device.char_read("f000aa01-0451-4000-b000-000000000000")    # temperature data UUID
+        value = device.char_read("f000aa71-0451-4000-b000-000000000000")    # luxometer data UUID
         print(f"Received data: {hexlify(value)}")
 finally:
     adapter.stop()
