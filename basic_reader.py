7a131ff7ebbcdea31bb68c2244ca2c70561dbffd
justin.mak <justin.mak@xylem.com>
2021-08-17 12:17:49 +0100

    Add sleep

--- basic_reader.py
+++ basic_reader.py
@@ -19,7 +19,9 @@ try:
     adapter.start()
     device = adapter.connect('80:6F:B0:F0:2B:95')   # target MAC address
     device.char_write("f000aa72-0451-4000-b000-000000000000", b'\x00')
+    time.sleep(0.5)
     device.char_write("f000aa72-0451-4000-b000-000000000000", b'\x01')
+
     while True:
         value = device.char_read(sensor_ids["lux_data"])
         print(struct.unpack("<f", value))
