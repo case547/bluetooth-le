8a3c76f2f5cc69b8f8b1c9ba55ffc1a904e30821
case547 <9521840+case547@users.noreply.github.com>
2021-08-18 17:34:16 +0100

    Create async_read.py

---
+++ optimyze/async_read.py
@@ -0,0 +1,11 @@
+from gattlib import GATTRequester, GATTResponse
+import time
+
+req = GATTRequester("DB:88:84:FD:CE:20")
+response = GATTResponse()
+
+req.read_by_uuid_async("e3290004-8862-42ae-9d81-e6e9ec0f5fdf", response)
+while not response.received():
+    time.sleep(0.1)
+
+steps = response.received()[0]
