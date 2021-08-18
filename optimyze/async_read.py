c0b2007eb69a11131848e41152a9cd4dcbb63a1b
case547 <9521840+case547@users.noreply.github.com>
2021-08-18 17:39:33 +0100

    Fix randomness

--- optimyze/async_read.py
+++ optimyze/async_read.py
@@ -1,10 +1,10 @@
 from gattlib import GATTRequester, GATTResponse
 import time
 
-req = GATTRequester("DB:88:84:FD:CE:20")
+req = GATTRequester("DB:88:84:FD:CE:20", False)
 response = GATTResponse()
 
-req.connect(False, channel_type='random')
+req.connect(True, channel_type='random')
 req.read_by_uuid_async("e3290004-8862-42ae-9d81-e6e9ec0f5fdf", response)
 
 while not response.received():
--- optimyze/notify_response.py
+++ optimyze/notify_response.py
@@ -6,8 +6,8 @@ class NotifyStatus(GATTResponse):
         print(f"Status: {status}")
 
 response = NotifyStatus()
-req = GATTRequester("DB:88:84:FD:CE:20")
-req.connect(False, channel_type='random')
+req = GATTRequester("DB:88:84:FD:CE:20", False)
+req.connect(True, channel_type='random')
 req.read_by_uuid_async("e3290004-8862-42ae-9d81-e6e9ec0f5fdf", response)
 
 while True:
