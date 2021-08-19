2b38c8675f4cfc7f98f2e708af064d1d03a9659b
case547 <9521840+case547@users.noreply.github.com>
2021-08-19 09:51:59 +0100

    Import from future module

--- optimyze/read_async.py
+++ optimyze/read_async.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import sys
 import time
 from gattlib import GATTRequester, GATTResponse
--- optimyze/receive_notification.py
+++ optimyze/receive_notification.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import sys
 from threading import Event
 from gattlib import GATTRequester
