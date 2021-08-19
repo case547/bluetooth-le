0dcbef429401ae3bf7eb9f1c3785c4ebf8c962ed
case547 <9521840+case547@users.noreply.github.com>
2021-08-19 13:36:23 +0100

    Add status check

--- optimyze/async_wait.py
+++ optimyze/async_wait.py
@@ -1,3 +1,4 @@
+from os import write
 from gattlib import GATTRequester, GATTResponse
 import time
 
@@ -5,7 +6,25 @@ req = GATTRequester("DB:88:84:FD:CE:20", False)
 response = GATTResponse()
 
 req.connect(True, channel_type='random')
-req.read_by_uuid_async("e3290004-8862-42ae-9d81-e6e9ec0f5fdf", response)
+
+print("Checking device availability...")
+
+for i in range(10):
+    status_char = req.read_by_uuid("e3290004-8862-42ae-9d81-e6e9ec0f5fdf")
+    busy_flag = bin(status_char[-1])[-1]
+    
+    if busy_flag == '0':
+        print("OK!")
+        break
+
+    if i == 9:
+        raise TimeoutError("Device is busy.")
+
+    time.sleep(1)
+
+req.write_by_uuid("e3290003-8862-42ae-9d81-e6e9ec0f5fdf", b'\x00\x1a')
+
+req.read_by_uuid_async("e3290002-8862-42ae-9d81-e6e9ec0f5fdf", response)
 
 print("Awaiting response")
 while not response.received():
