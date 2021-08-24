a8eb908187bfe1d0e6c74c28896dc19243d87632
case547 <9521840+case547@users.noreply.github.com>
2021-08-24 17:03:46 +0100

    Update read script

--- ti-lpstk/read_name.py
+++ ti-lpstk/read_name.py
@@ -2,12 +2,12 @@ import asyncio
 from bleak import BleakClient
 
 address = "80:6F:B0:F0:2B:95"
-MODEL_NBR_UUID = "00002a24-0000-1000-8000-00805f9b34fb"
+DEV_NAME_UUID = "00002a00-0000-1000-8000-00805f9b34fb"
 
 async def run(address):
     async with BleakClient(address) as client:
-        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-        print("Model Number: {0}".format("".join(map(chr, model_number))))
+        device_name = await client.read_gatt_char(DEV_NAME_UUID)
+        print("Device name:", device_name.decode("utf-8"))
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run(address))
